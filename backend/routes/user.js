8551ecdb82c8f196bf3bb32c33e8156731fedb88
uirajeev <rajeevkumar.web@gmail.com>
2023-09-05 08:10:45 +0530

    Login route created

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { home, register }  from '../controller/user.js';
+import { home, register, activateAccount, login }  from '../controller/user.js';
 
 const router = Router();
 
 router.get('/', home);
 router.post('/register', register);
+router.post('/activate', activateAccount);
+router.post('/login', login);
 
 export default router;
