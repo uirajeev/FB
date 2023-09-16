d7b50e641cd05b43150bd4a2cb7610e1d52d980d
uirajeev <rajeevkumar.web@gmail.com>
2023-09-16 21:51:48 +0530

    Outsideclick huck added.

---
+++ frontend/src/helpers/ClickOutside.js
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+const ClickOutside = (ref, fun) => {
+  useEffect(() => {
+    const listener = (e) => {
+      if (!ref.current || ref.current.contains(e.target)) {
+        return;
+      }
+      fun();
+    };
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref]);
+};
+
+export default ClickOutside;
--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,9 +1,16 @@
+import { useRef } from 'react';
+import useClickOutside from '../../helpers/ClickOutside';
 import Header from '../../components/header';
 
 const Home = () => {
+  const el = useRef(null);
+  useClickOutside(el, () => {
+    el.current.style.display = 'none';
+  });
   return (
     <div>
       <Header />
+      <div className="card" ref={el}></div>
     </div>
   );
 };
